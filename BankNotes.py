309224cae2daff64d318464137f232d348c56e56
Zeyad Fouad Salem Elsaied <62177326+DevZeyad@users.noreply.github.com>
2020-03-14 15:40:14 +0200

    Update BankNotes.py

--- BankNotes.py
+++ BankNotes.py
@@ -1,6 +1,7 @@
 #This is an uri online judge problem solution
 #you have to read int value and calculate the smallest possible number of bank notes in which the value may be decomposed
 #Actually , loved this idea :D
+#So i decided to post my solution as a first github code for me
 
 availableNotas = [100.00,50.00,20.00,10.00,5.00,2.00,1.00]
 Amount = int(input("Enter The Amount Please: "))
